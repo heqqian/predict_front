18f624cc2b9ef75345029b2893997ecf57ac7b0a
heqqqqqqq <2841872640@qq.com>
2022-09-11 17:14:00 +0800

    add file choose

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,39 @@
 require('./index.css')
 
-var fun=function(){
-    console.log('index:index.js')
-}
 
-module.exports=fun;
+function upload(){
+    var fileObj=document.createElement('input');
+    fileObj.setAttribute('id','file');
+    fileObj.setAttribute('type','file');
+    fileObj.setAttribute('name','file');
+    fileObj.setAttribute('style','visibility:hidden');
+    document.body.appendChild(fileObj);
+    fileObj.value;
+    fileObj.click();
+    console.log(fileObj);
+}
+//选择数据集
+$('#fileChooseBtn1').on('click',function(){
+    if(document.getElementById('file')){
+        document.getElementById('file').remove();
+    }
+    upload();
+    document.querySelector('#file').addEventListener('change',e=>{
+        for(let entry of e.target.files){
+            document.getElementById('filename1').value=entry.name;
+            console.log(entry.name,entry.webkitRelativePath);
+        }
+    })
+})
+$('#fileChooseBtn2').on('click',function(){
+    if(document.getElementById('file')){
+        document.getElementById('file').remove();
+    }
+    upload();
+    document.querySelector('#file').addEventListener('change',e=>{
+        for(let entry of e.target.files){
+            document.getElementById('filename2').value=entry.name;
+            console.log(entry.name,entry.webkitRelativePath);
+        }
+    })
+})
